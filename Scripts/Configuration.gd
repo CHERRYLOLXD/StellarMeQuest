582c12a706889af16be1fcb27a34dca4a697dce2
CHERRYLOLXD <ty1ta1ti@gmail.com>
2023-10-06 13:02:12 +0700

    NEW

--- Scripts/Configuration.gd
+++ Scripts/Configuration.gd
@@ -1,7 +1,7 @@
 extends Control
 
-func _on_close_configuration_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
-
 func _on_ready():
 	pass
+
+func _on_close_configuration_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
